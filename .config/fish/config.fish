3b50754e32155a021bb02ec79cf9048a430e9bd3
jpaquim <joaopaquim@gmail.com>
2019-08-06 22:39:23 +0100

    Add PostgreSQL and Dart packages to $PATH
    Override default system locale for consistency in documentation language

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,11 @@
+# Disable the default greeting
 set fish_greeting
+
+# Override system locale for consistency
+set -gx LC_ALL en_US.UTF-8
+
+# PostgreSQL
+set -gx PATH /Applications/Postgres.app/Contents/Versions/latest/bin $PATH
+
+# Dart packages
+set -gx PATH /Users/jpaquim/.pub-cache/bin $PATH
