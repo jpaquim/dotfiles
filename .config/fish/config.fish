efc07d68c6e4f460ec8c6b8241179608664dfb28
jpaquim <joaopaquim@gmail.com>
2019-11-13 10:07:47 +0000

    Add aliases to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,3 +9,12 @@ set -gx PATH /Applications/Postgres.app/Contents/Versions/latest/bin $PATH
 
 # Dart packages
 set -gx PATH /Users/jpaquim/.pub-cache/bin $PATH
+
+# Aliases
+alias g "git"
+
+# jq aliases to get package.json fields
+alias deps "jq .dependencies package.json | jq keys[]"
+alias depsv "jq .dependencies package.json"
+alias scripts "jq .scripts package.json"
+alias version "jq .version package.json"
